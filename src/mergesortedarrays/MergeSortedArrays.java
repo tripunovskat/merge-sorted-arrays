3ac1c0e0517ab08e616cfa8e557645596014a7eb
Lenovo <Lenovo@DESKTOP-B67EL13>
2020-05-21 08:18:37 +0200

    new commit

---
+++ src/mergesortedarrays/MergeSortedArrays.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mergesortedarrays;
+
+/**
+ *
+ * @author Tripunovska Tina
+ */
+public class MergeSortedArrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    /**
+     * mergeSortedArrays([0,3,4,31], [3,4,6,30]);
+     * merge these arrays in one sorted array
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static int[] mergeAndSort(int[] array1, int[] array2) {
+        //TODO
+        return array1;
+    }
+    
+}
