7f33301d4b80c7284bd55ae21167d1cae8197115
Lenovo <Lenovo@DESKTOP-B67EL13>
2020-05-21 21:11:44 +0200

    merged arrays

--- src/mergesortedarrays/MergeSortedArrays.java
+++ src/mergesortedarrays/MergeSortedArrays.java
@@ -1,31 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mergesortedarrays;
+
+import java.util.Arrays;
 
 /**
  *
  * @author Tripunovska Tina
+ *
  */
 public class MergeSortedArrays {
 
     /**
      * @param args the command line arguments
      */
-    
-    /**
-     * mergeSortedArrays([0,3,4,31], [3,4,6,30]);
-     * merge these arrays in one sorted array
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[] array1 = {0, 1, 4, 13, 33};
+        int[] array2 = {0, 2, 3, 5, 22, 30, 34, 50};
+        mergeSortedArrays(array1, array2);
+
     }
-    
-    public static int[] mergeAndSort(int[] array1, int[] array2) {
-        //TODO
-        return array1;
+
+    public static int[] mergeSortedArrays(int[] array1, int[] array2) {
+        int[] mergedArray = new int [array1.length + array2.length];
+        int array1Length = array1.length;
+        int array2Length = array2.length;
+        int i = 0;
+        int j = 0;
+        int m = 0;
+
+        if (array1Length == 0) {
+            return array2;
+        }
+        if (array2Length == 0) {
+            return array1;
+        }
+
+        while (i < array1Length && j < array2Length) {
+            if (array1[i] <= array2[j]) {
+                mergedArray[m] = array1[i];
+                i++;
+            } else {
+                mergedArray[m] = array2[j];
+                j++;
+            }
+            m++;
+        }
+        
+        while (i < array1Length) 
+            mergedArray[m++] = array1[i++]; 
+
+        while (j < array2Length) 
+            mergedArray[m++] = array2[j++]; 
+        
+        System.out.println(Arrays.toString(mergedArray));
+        return mergedArray;
     }
-    
+
 }
